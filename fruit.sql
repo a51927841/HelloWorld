c6b501695a2661f74ce88f40317cc27ce291ea9e
a51927841 <51927841@qq.com>
2016-07-19 14:37:07 +0800

    45
    456

---
+++ fruit.sql
@@ -0,0 +1,6 @@
+INSERT INTO `fruit` VALUES (1, '苹果', '黄色', '大');
+INSERT INTO `fruit` VALUES (2, '苹果', '红色', '中');
+INSERT INTO `fruit` VALUES (3, '苹果', '黄色', '小');
+INSERT INTO `fruit` VALUES (4, '桔子', '黄色', '大');
+INSERT INTO `fruit` VALUES (5, '桔子', '红色', '小');
+INSERT INTO `fruit` VALUES (6, '桔子', '黄色', '小');
